7287f40db13c462384a80b4e21807165fc4ade8c
Programmer-Aiims <100011059+Programmer-Aiims@users.noreply.github.com>
2023-05-22 21:00:08 +0530

    Update my_app.py

--- my_app.py
+++ my_app.py
@@ -7,8 +7,8 @@ import streamlit as st
 # Uses st.cache_data to only rerun when the query changes or after 10 min.
 @st.cache_data(ttl=600)
 def load_data(sheets_url):
-    sheet_url = “https://docs.google.com/spreadsheets/d/1shPozJI4zs2HuY9P7MWoQzPj1pmrPy8NnJUQ-aScYY0/edit#gid=0"
-    url_1 = sheet_url.replace(‘/edit#gid=’, ‘/export?format=csv&gid=’)
+    sheet_url = "https://docs.google.com/spreadsheets/d/1shPozJI4zs2HuY9P7MWoQzPj1pmrPy8NnJUQ-aScYY0/edit#gid=0"
+    url_1 = sheet_url.replace('/edit#gid=', '/export?format=csv&gid=')
     #csv_url = sheets_url.replace("/edit#gid=", "/export?format=csv&gid=")
     #print(csv_url)
     return pd.read_csv(url_1)
